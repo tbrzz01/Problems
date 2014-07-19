cd4f2f134f910eb7f3b62d3dcbbf7b19bf454802
Trey Briggs <tebriggs86@gmail.com>
2014-07-19 11:27:50 -0500

    Adding implementation and Unit test for implementation

--- GoogleInterview/src/Main.java
+++ GoogleInterview/src/Main.java
@@ -18,8 +18,37 @@ public class Main {
 
 	}
 	
+	public static void TestBuildStrings(List<List<String>> list){
+		buildStrings(list);
+	}
+	
+	/**
+	 * @param list 
+	 */
 	private static void buildStrings(List<List<String>> list){
 		// TODO implemenet
+		if(list != null && list.size() > 0){
+			traverse("", list, 0, list.size());
+		}
 	}
+	
+	/**
+	 * 
+	 * @param s
+	 * @param strings
+	 * @param currentLevel
+	 * @param maxLevel
+	 */
+	private static void traverse(String s, List<List<String>> strings, int currentLevel, int maxLevel){
+		if(currentLevel >= maxLevel){
+			System.out.println(s);
+		}
+		else{
+			for(String word : strings.get(currentLevel)){
+				traverse(s + " " + word, strings, currentLevel + 1, maxLevel);
+			}
+		}
+	}
+	
 
 }
