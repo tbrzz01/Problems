b81c9641739dadcaf7c72ac6da0dd183498ff8ff
Trey Briggs <tebriggs86@gmail.com>
2014-07-19 10:46:19 -0500

    Initial commit of implementation to problem given in google interview

---
+++ GoogleInterview/README.md
@@ -0,0 +1,1 @@
+This project represents my solution to my Google interview.
---
+++ GoogleInterview/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author Trey
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void buildStrings(List<List<String>> list){
+		// TODO implemenet
+	}
+
+}
