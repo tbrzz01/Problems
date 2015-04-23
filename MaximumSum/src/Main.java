78ea429007fe1eddb4fbf547d0dcfed0aef18d3d
Trey Briggs <tebriggs86@gmail.com>
2015-04-22 21:03:15 -0500

    Adding initial java project

---
+++ MaximumSum/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Trey
+ * Problem : http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=44
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
